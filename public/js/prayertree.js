65f959a659c60bd0b1624627ba5b3d7ca232ca34
Jonathan Berry <jonathan.s.berry@googlemail.com>
2016-10-22 22:45:02 +0100

    Finished prayer tree editor.  Needs styling and linking to backend.

--- public/js/prayertree.js
+++ public/js/prayertree.js
@@ -123,11 +123,11 @@ function drawTables( prayertree_id ){
         drawPrayerRequest($element, 'data-container');
     });
 
-    /*Insert data into editor when clicking upon a row.
+    //Insert data into editor when clicking upon a row.
     $( '#contact-table' ).on('click-row.bs.table', function (row, $element, field) {
         console.log( "clicked" );
-        drawContact($element, 'request-container');
-    });*/
+        drawContactEditor($element, 'data-container');
+    });
 
     $( '.tables .bootstrap-table .fixed-table-toolbar' ).prepend( 
         "<div class='btn btn-group pull-left columns'> <button class='requests btn btn-large' >" +
@@ -135,15 +135,29 @@ function drawTables( prayertree_id ){
         " Contacts </button> </div>"
     ); 
 
-    //Add extra toolbar buttons
-    $('.tables .bootstrap-table .fixed-table-toolbar').append( 
-        "<div class='btn-group pull-right table-custom-toolbar columns'>" + 
-        "<button class='btn highlight delete-users' type='button'>" +
-        "<span class='glyphicon glyphicon-trash'/></button>" +
-        "<button class='btn blue new-user' type='button'>" +
-        "<span class='glyphicon glyphicon-plus'/></button>" +
-        "</div>"
+    //Add +/- request buttons
+    $('.request-container .bootstrap-table .fixed-table-toolbar').append( 
+      "<div class='btn-group pull-right table-custom-toolbar columns'>" + 
+      "<button class='btn highlight delete-request' type='button'>" +
+      "<span class='glyphicon glyphicon-trash'/></button>" +
+      "<button class='btn blue new-request' type='button'>" +
+      "<span class='glyphicon glyphicon-plus'/></button>" +
+      "</div>"
     );
+
+    //Add +/- contact buttons
+    $('.contact-container .bootstrap-table .fixed-table-toolbar').append( 
+      "<div class='btn-group pull-right table-custom-toolbar columns'>" + 
+      "<button class='btn highlight delete-contact' type='button'>" +
+      "<span class='glyphicon glyphicon-trash'/></button>" +
+      "<button class='btn blue new-contact' type='button'>" +
+      "<span class='glyphicon glyphicon-plus'/></button>" +
+      "</div>"
+    );
+
+    $('.table-custom-toolbar .new-contact').click( function(){
+        drawContactEditor(undefined, 'data-container');
+    });
 
     $('.fixed-table-toolbar .requests').click( function(){
 
@@ -202,9 +216,122 @@ function drawPrayerRequest(request, container_id){
 
 }
 
-function drawContactEditor(contact_id){
+function drawContactEditor(contact, container_id){
+
+    //Initialise form to empty if no contact specified.
+    if( contact == null ){
+        contact = {
+            'type':'email',
+            'name':'',
+            'value':''
+        }
+    }
+
+    var html = "<div class='contacteditor panel col-md-8 col-md-offset-2'>";
+
+    html += "<form class='contacteditor__form'>";
+    
+    html += "<div class='form-group col-xs-12 col-sm-6 col-md-6'>";
+
+    html += "<div class='input-group row'>" + 
+            "<label for='type' class='type col-xs-12 col-md-6 col-lg-5'>Type: " + 
+            "</label><select class='form-control type col-xs-12 col-md-6 col-lg-7' " +
+            "name='type' value='" + contact.type + "' id='type' required oninput='updateType();'>"+
+            "<option value='email' selected>Email</option>" +
+            "<option value='sms'>SMS</option>" +
+            "</select></div>";
+
+    html += "<div class='input-group row'>" + 
+            "<label class='name form-control  col-xs-12 col-md-6 col-lg-5'>Name: " + 
+            "</label><input type='text' class='username col-xs-12 col-md-6 col-lg-7' " +
+            "name='name' value='" + contact.name + "' id='name' required /></div>";
+
+    html += "<div class='input-group row'>" +
+            "<label class='email form-control col-xs-12 col-md-6 col-lg-5'><span class='type-word'>"+
+            "Email:</span></label>" + 
+            "<input id='value' type='text' class='value col-xs-12 col-md-6 col-lg-7' " +
+            "oninput='checkEqual(\"value\", \"value2\")' name='value' value='" + 
+            contact.value + "' required/></div>";
+
+    html += "<div class='input-group row'>" + 
+            "<label class='email2 form-control  col-xs-12 col-md-6 col-lg-5'>" + 
+            "Retype <span class='type-word'>Email</span>:</label>" + 
+            "<input  id='value2' type='text' oninput='checkEqual(\"value\", \"value2\")' " +
+            "class='value2 col-xs-12 col-md-6 col-lg-7' value='" + 
+            contact.value + "' autocomplete='off' required /></div></div>";
+
+    html += "<button type='submit' class='col-xs-12 col-sm-6 btn btn-large " +
+            "submit-form'>Submit</button>";
+
+    html += "</form>";
+
+    html += "</div>";
+
+    $('#'+container_id).html( html );
+
+    $('.contacteditor__form .type').val(contact.type);
+
 
     
 
+    //FORM SUBMISSION
+    $('.contacteditor__form').click(function(evt){
+        /*
+        evt.preventDefault();
+
+        //Assemble complete json object.
+        var data = {};
+        var form = $('.user-editor');
+        var formArray = form.serializeArray();
+
+        for( var z in formArray ){
+            var element = formArray[z];
+            data[element.name] = element.value;
+        }
+
+        $.extend( data, extractAccess() );
+        $.extend( data, extractData() );
+
+        //Post json to server.
+        $.ajax({
+            url: root + '/en/users/update_user/' + data.original_username,
+            type: 'post',
+            success: function (data) {
+                alert(data);
+                drawUserEditor("");
+                $('#user-table table').bootstrapTable('refresh');
+            },
+            error: function (data) {
+                alert( i18n.gettext("There has been a server error. " + 
+                                    "Please contact administrator and try again later.") );
+                $('.user-editor .submit-form').text( buttonText );
+                $('#user-table table').bootstrapTable('refresh');
+            },
+            contentType: 'application/json;charset=UTF-8',
+            data: JSON.stringify(data, null, '\t')
+        });*/
+        
+    });
+
+}
+
+function updateType(){
+    console.log( 'flag2');
+    $('.type-word').text( $('.contacteditor__form .type').val().replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}));
+}
+
+function checkEqual( id1, id2 ) {
+
+    var email1 = document.getElementById(id1);
+    var email2 = document.getElementById(id2);
+    console.log( email1 == email2 );
+    if ( email2.value != email1.value ) {
+        email2.setCustomValidity( 'Fields must be Matching.' );
+        return false;
+    } else {
+        //Input is valid -- reset the error message
+        email2.setCustomValidity('');
+        return true;
+    }
 }
 
