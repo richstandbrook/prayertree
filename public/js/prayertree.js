abef528e7e4cd30b1649a1ae076adf149b5dd112
Jonathan Berry <jonathan.s.berry@googlemail.com>
2016-10-22 20:52:25 +0100

    Two tables.

--- public/js/prayertree.js
+++ public/js/prayertree.js
@@ -1,8 +1,9 @@
 var apipath = "/mock-api/";
 
-function drawPrayerRequestTable( container_id, prayertree_id ){
+function drawTables( prayertree_id ){
+
     //Define the table structure
-    var columns = [{
+    var requestColumns = [{
             'field': 'state',
             'checkbox': true,
             'align': 'center',
@@ -11,35 +12,65 @@ function drawPrayerRequestTable( container_id, prayertree_id ){
             'field': "status",
             'title': "State",
             'align': "left",
-            'class': "header",
+            'class': "header clickable",
             'sortable': true,
             'width': "10%"
         },{
             'field': "contact_name",
             'title': "From",
             'align': "left",
-            'class': "header",
+            'class': "header clickable",
             'sortable': true,
             'width': "15%"
         },{
             'field': "time_received",
             'title': "Time",
             'align': "left",
-            'class': "header",
+            'class': "header clickable",
             'sortable': true,
             'width': "15%"
         },{
             'field': "text",
             'title': "Message",
             'align': "left",
-            'class': "header",
+            'class': "header clickable",
             'sortable': false,
             'width': "60%"
         }         
     ];
 
+    //Define the table structure
+    var contactColumns = [{
+            'field': 'state',
+            'checkbox': true,
+            'align': 'center',
+            'valign': 'middle'
+        },{
+            'field': "type",
+            'title': "Type",
+            'align': "left",
+            'class': "header",
+            'sortable': true,
+            'width': "20%"
+        },{
+            'field': "name",
+            'title': "Name",
+            'align': "left",
+            'class': "header",
+            'sortable': true,
+            'width': "30%"
+        },{
+            'field': "value",
+            'title': "Contact",
+            'align': "left",
+            'class': "header",
+            'sortable': true,
+            'width': "50%"
+        }        
+    ];
+
     //A function that prepares the data for displaying in the table.
-    function prepData(res){
+    function requestPrep(res){
         console.log(res);
         for( var a in res.requests ){
             var row = res.requests[a];
@@ -48,19 +79,132 @@ function drawPrayerRequestTable( container_id, prayertree_id ){
         }
         return res.requests;
     }
+
+    //A function that prepares the data for displaying in the table.
+    function contactPrep(res){
+        console.log(res);
+        for( var a in res.contacts ){
+            var row = res.contacts[a];
+            //TODO Prepdata.
+        }
+        return res.contacts;
+    }
     
-    var tmp = apipath + 'prayer_requests/' + prayertree_id;
-    console.log( "tmp: " + tmp );
+    var requestUrl = apipath + 'prayer_requests/' + prayertree_id;
+    var contactUrl = apipath + 'contacts/' + prayertree_id;
 
     //Creat the bootstrap table.
-    table = $('#'+container_id).bootstrapTable({
-        columns: columns,
+    table = $('#request-table').bootstrapTable({
+        columns: requestColumns,
         classes: 'table table-hover',
         pagination: true,
         pageSize: 20,
         search: true,
-        url: tmp,
-        responseHandler: prepData,
+        url: requestUrl,
+        responseHandler: requestPrep,
         showRefresh: true
     });
+
+    //Create the bootstrap table.
+    table = $('#contact-table').bootstrapTable({
+        columns: contactColumns,
+        classes: 'table table-hover',
+        pagination: true,
+        pageSize: 20,
+        search: true,
+        url: contactUrl,
+        responseHandler: contactPrep,
+        showRefresh: true
+    });
+
+    //Insert data into editor when clicking upon a row.
+    $( '#request-table' ).on('click-row.bs.table', function (row, $element, field) {
+        console.log( "clicked" );
+        drawPrayerRequest($element, 'data-container');
+    });
+
+    /*Insert data into editor when clicking upon a row.
+    $( '#contact-table' ).on('click-row.bs.table', function (row, $element, field) {
+        console.log( "clicked" );
+        drawContact($element, 'request-container');
+    });*/
+
+    $( '.tables .bootstrap-table .fixed-table-toolbar' ).prepend( 
+        "<div class='btn btn-group pull-left columns'> <button class='requests btn btn-large' >" +
+        " Requests </button> <button class='contacts btn btn-large' >" +
+        " Contacts </button> </div>"
+    ); 
+
+    //Add extra toolbar buttons
+    $('.tables .bootstrap-table .fixed-table-toolbar').append( 
+        "<div class='btn-group pull-right table-custom-toolbar columns'>" + 
+        "<button class='btn highlight delete-users' type='button'>" +
+        "<span class='glyphicon glyphicon-trash'/></button>" +
+        "<button class='btn blue new-user' type='button'>" +
+        "<span class='glyphicon glyphicon-plus'/></button>" +
+        "</div>"
+    );
+
+    $('.fixed-table-toolbar .requests').click( function(){
+
+        $('.contact-container').hide();
+        $('.request-container').show();
+        $('.fixed-table-toolbar .contacts').toggleClass('active');
+        $('.fixed-table-toolbar .requests').toggleClass('active');
+
+    });
+
+    $('.fixed-table-toolbar .contacts').click( function(){
+
+        $('.request-container').hide();
+        $('.contact-container').show();
+        $('.fixed-table-toolbar .contacts').toggleClass('active');
+        $('.fixed-table-toolbar .requests').toggleClass('active');
+
+    });
+
+    $('.contact-container').hide();
+    $('.fixed-table-toolbar .requests').addClass('active');
+
 }
+
+function drawPrayerRequest(request, container_id){
+
+    var html = "<div class='request col-md-8 col-md-offset-2'>";
+
+    html += "<div class='request__top'>";
+    
+    html += "<div class='request__from'><b>From: </b>" + 
+            request.contact_name + "</div>" ;
+
+    html += "<div class='request__when pull-right'>" + 
+            request.time_received + "</div>" ;
+
+    html += "</div>";
+
+    html += "<div class='request__main'>" + request.text + "</div>";
+
+    html += "<div class='request__bottom'>";
+
+    html += "<div class='btn-group pull-right'>";
+
+    html += "<button class='btn btn-large btn-success'> Approve </button>";
+
+    html += "<button class='btn btn-large btn-danger'> Remove </button>";
+
+    html += "</div>";
+
+    html += "</div>";
+
+    html += "</div>";
+
+    $('#'+container_id).html( html );
+
+}
+
+function drawContactEditor(contact_id){
+
+    
+
+}
+
--- resources/views/prayertree.blade.php
+++ resources/views/prayertree.blade.php
@@ -5,13 +5,21 @@
     <div class="row">
         <div class="col-md-12">
             <h1 class="panel-heading">
-			    Edit PrayerTree: <?php echo( $prayertree_id ); ?> 
+                <span class='prayertree-name'>
+                    <?php echo( $prayertree_id ); ?>
+                </span> 
 		    </h1>
         </div>
     </div>
     <div class="row">
-        <div class="panel col-md-12">
-	        <div id='prayer-request-table'></div>
+        <div class="col-md-12">
+	        <div id='data-container'></div>
+        </div>
+    </div>
+    <div class="tables-container panel">
+        <div class="row tables">
+            <div class='request-container prayertree-table'><div id='request-table'></div></div>
+            <div class='contact-container prayertree-table'><div id='contact-table'></div></div>
         </div>
     </div>
 </div>
@@ -19,7 +27,18 @@
 
 @section('scripts')
 <script>
-drawPrayerRequestTable( 'prayer-request-table', '<?php echo( $prayertree_id ); ?>' );
+$.getJSON( '/prayertrees', function(data){
+    console.log( data );
+    for( var i=0; i<data.length; i++ ){
+        console.log( data[i].pin  );
+        console.log( '<?php echo( $prayertree_id ); ?>'  );
+        if( data[i].pin == '<?php echo( $prayertree_id ); ?>' ){
+            $('.prayertree-name').text( data[i].name );
+            break;
+        }
+    }
+});
 
+drawTables('<?php echo( $prayertree_id ); ?>' );
 </script>
 @endsection
